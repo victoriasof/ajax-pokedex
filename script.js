6ac7c7dc316e53e01dafb2d61489ef329950accf
Victoria <victoria.sofianidou@gmail.com>
2020-09-14 14:51:14 +0200

    messing around with javascript

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+/*
+search pokémon by name and ID
+show:
+The ID-number
+An image (sprite)
+At least 4 "moves"
+The previous evolution
+ */
 
+(() => {
+    // your code here
+    document.getElementById("search").addEventListener("click", () => {
 
+        fetch("https://pokeapi.co/api/v2/pokemon/")
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .then(data => searchData(data))
+            .catch(err => console.error(err));
 
-fetch("https://pokeapi.co/api/v2/pokemon/")
-    .then(response => response.json())
-    .then(data => console.log(data))
+        function searchData(pokemon) {
 
-    
+            let pokemonId = Number(document.getElementById("pokeId").value);
+            let pokemonName = Number(document.getElementById("name").value);
 
-    .catch(err => console.error(err));
+            document.getElementById("pokeId").innerHTML = pokemon.id;
+            document.getElementById("name").innerHTML = pokemon.types[0].type.name;
+            document.getElementById("sprite").innerHTML = pokemon.sprites.front_default;
+            document.getElementById("moves").innerHTML = pokemon.moves;
+
+        }
+
+    })
+})();
